a4ad07397ff32124b6fb343366ed5eeab34f50a7
James Botwina <34082590+JBotwina@users.noreply.github.com>
2020-04-11 15:24:22 -0400

    Update settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' 
-EMAIL_HOST_PASSWORD =  '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'
+EMAIL_HOST_PASSWORD =  'private'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
