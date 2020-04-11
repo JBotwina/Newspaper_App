73897f29d928b41500a170f820ec754e99bbdbdb
Jamesbot16 <jimbotwina16@gmail.com>
2020-04-10 20:56:58 -0400

    added comments to the article without the admin site

--- articles/models.py
+++ articles/models.py
@@ -10,8 +10,6 @@ class Article(models.Model):
     author = models.ForeignKey(
         get_user_model(),
         on_delete = models.CASCADE,
-        null=True,
-        blank=True,
     )
 
     def __str__(self):
@@ -26,9 +24,8 @@ class Comment(models.Model):
         Article,
         on_delete= models.CASCADE,
         related_name='comments',
-        null=True,
-        blank=True,
         )
+
     comment = models.CharField(max_length =140)
 
     approved_comment = False
@@ -36,18 +33,11 @@ class Comment(models.Model):
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE, 
-        null=True,
-        blank=True,
 
     )
     def __str__(self):
         return self.comment
-    '''
-    def approve(self):
-        self.approved_comment = True
-        self.save()
-    '''
+   
     def get_absolute_url(self):
-        self.save()
         return reverse('article_list')
 
--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
 )
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -55,7 +55,12 @@ class ArticleCommentView(CreateView):
     fields = ('comment', )
     template_name = "article_comment.html"
     success_url = reverse_lazy('article_list')
-    login_url = 'login'
+    
+    def form_valid(self,form):
+        article = get_object_or_404(Article, pk=self.kwargs.get('pk'))
+        form.instance.author = self.request.user
+        form.instance.article =article
+        return super().form_valid(form)
     
 
     
